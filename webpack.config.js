514d8591c282b6992ed0d30afa507ee78ff4c138
Alex Schwarz <as@active-value.de>
2019-03-28 22:06:22 +0100

    added tailwind config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/styles.css',
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1, minimize: true } },
+            'postcss-loader',
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      disable: process.env.NODE_ENV === 'development',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+  ],
+}
